f1ae64dfe24b901ba839774c364e2b185c1a57a9
TzuHungCheng <jeff.cheng@rocketmail.com>
2025-06-01 23:48:12 -0500

    property details view

--- hotproperty/src/main/java/edu/finalproject/hotproperty/controllers/BuyerController.java
+++ hotproperty/src/main/java/edu/finalproject/hotproperty/controllers/BuyerController.java
@@ -2,6 +2,10 @@ package edu.finalproject.hotproperty.controllers;
 
 import edu.finalproject.hotproperty.entities.*;
 import edu.finalproject.hotproperty.repositories.*;
+import edu.finalproject.hotproperty.services.AuthServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -9,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -21,6 +26,8 @@ public class BuyerController {
     private final PropertyImageRepository propertyImageRepository;
     private final MessageRepository messageRepository;
     private final FavoriteRepository favoriteRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
 
     @Autowired
     public BuyerController(UserRepository userRepository, PropertyRepository propertyRepository, PropertyImageRepository propertyImageRepository, MessageRepository messageRepository, FavoriteRepository favoriteRepository) {
@@ -86,5 +93,13 @@ public class BuyerController {
         model.addAttribute("sortBy", sortBy);
 
         return "/buyer/browse_properties";
+    }
+
+    @PreAuthorize("hasRole('BUYER')")
+    @GetMapping("/properties/view/{id}")
+    public String viewPropertyDetail(@PathVariable Long id, Model model) {
+        Property property = propertyRepository.findWithImagesById(id).orElseThrow();
+        model.addAttribute("property", property);
+        return "/buyer/property_details_view";
     }
 }
--- hotproperty/src/main/java/edu/finalproject/hotproperty/repositories/PropertyRepository.java
+++ hotproperty/src/main/java/edu/finalproject/hotproperty/repositories/PropertyRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
@@ -14,4 +15,7 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
   // will need more methods for searching, filtering, etc.
   @EntityGraph(attributePaths = "images")
   List<Property> findAll();
+
+  @EntityGraph(attributePaths = "images")
+  Optional<Property> findWithImagesById(Long id);
 }
