6691955017a766d99a41d25fa57c2e3c388bda89
TzuHungCheng <jeff.cheng@rocketmail.com>
2025-06-02 22:43:29 -0500

    add filtering feature for browse properties

---
+++ hotproperty/src/main/java/edu/finalproject/hotproperty/services/PropertyServiceImpl.java
@@ -0,0 +1,43 @@
+package edu.finalproject.hotproperty.services;
+
+import edu.finalproject.hotproperty.entities.Property;
+import edu.finalproject.hotproperty.exceptions.InvalidPropertyParameterException;
+import edu.finalproject.hotproperty.repositories.PropertyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PropertyServiceImpl implements PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(PropertyServiceImpl.class);
+
+    @Autowired
+    public PropertyServiceImpl (PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
+    @Override
+    public Property findWithImagesById(Long id) {
+        return propertyRepository.findWithImagesById(id);
+    }
+
+    @Override
+    public List<Property> filterProperties(String zip, Integer minSqFt, Double minPrice, Double maxPrice, String sortBy) {
+        if (minPrice != null && maxPrice != null && minPrice > maxPrice) {
+            log.warn("Minimum price {} cannot be greater than maximum price {}.", minPrice, maxPrice);
+            throw new InvalidPropertyParameterException("Minimum price cannot be greater than maximum price.");
+        }
+
+        if (sortBy.equals("asc")) {
+            return propertyRepository.filterPropertiesOrderByAsc(zip, minSqFt, minPrice, maxPrice);
+        } else {
+            return propertyRepository.filterPropertiesOrderByDesc(zip, minSqFt, minPrice, maxPrice);
+        }
+    }
+}
