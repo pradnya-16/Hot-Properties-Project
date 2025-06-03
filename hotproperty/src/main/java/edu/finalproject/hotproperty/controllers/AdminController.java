d1b9250936c86d8855b5c8d31437a67e6d059272
samuel-ouzounian <samuelouzounian@gmail.com>
2025-06-03 16:31:28 -0500

    fix admin redirect & pre auth

--- hotproperty/src/main/java/edu/finalproject/hotproperty/controllers/AdminController.java
+++ hotproperty/src/main/java/edu/finalproject/hotproperty/controllers/AdminController.java
@@ -3,6 +3,7 @@ package edu.finalproject.hotproperty.controllers;
 import edu.finalproject.hotproperty.entities.User;
 import edu.finalproject.hotproperty.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,28 +14,31 @@ import java.util.List;
 @RequestMapping("/users/admin")
 public class AdminController {
 
-    private final AdminService adminService;
+  private final AdminService adminService;
 
-    @Autowired
-    public AdminController(AdminService adminService) {
-        this.adminService = adminService;
-    }
+  @Autowired
+  public AdminController(AdminService adminService) {
+    this.adminService = adminService;
+  }
 
-    @GetMapping
-    public String listUsers(Model model) {
-        List<User> users = adminService.getAllUsers();
-        model.addAttribute("users", users);
-        return "admin/user-list";
-    }
+  @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
+  public String listUsers(Model model) {
+    List<User> users = adminService.getAllUsers();
+    model.addAttribute("users", users);
+    return "admin/user-list";
+  }
 
-    @PostMapping("/delete/{userId}")
-    public String deleteUser(@PathVariable Long userId) {
-        adminService.deleteUser(userId);
-        return "redirect:/users/admin";
-    }
+  @PostMapping("/delete/{userId}")
+  @PreAuthorize("hasRole('ADMIN')")
+  public String deleteUser(@PathVariable Long userId) {
+    adminService.deleteUser(userId);
+    return "redirect:/users/admin";
+  }
 
-    @GetMapping("/create-agent")
-    public String createAgentForm() {
-        return "admin/agent_form";
-    }
+  @GetMapping("/create-agent")
+  @PreAuthorize("hasRole('ADMIN')")
+  public String createAgentForm() {
+    return "admin/agent_form";
+  }
 }
