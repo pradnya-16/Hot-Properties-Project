c71a24b5fa38b4960a6608a72806cf05525d1eb5
samuel-ouzounian <samuelouzounian@gmail.com>
2025-06-08 19:51:40 -0500

    fix delete

--- hotproperty/src/main/java/edu/finalproject/hotproperty/repositories/MessageRepository.java
+++ hotproperty/src/main/java/edu/finalproject/hotproperty/repositories/MessageRepository.java
@@ -3,12 +3,13 @@ package edu.finalproject.hotproperty.repositories;
 import edu.finalproject.hotproperty.entities.Message;
 import edu.finalproject.hotproperty.entities.Property;
 import edu.finalproject.hotproperty.entities.User;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
@@ -24,4 +25,8 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
   @EntityGraph(attributePaths = {"sender", "property"})
   Optional<Message> findWithSenderAndPropertyById(Long id);
 
+  @Query(
+      "SELECT COUNT(m) FROM Message m WHERE m.receiver = :agent AND (m.reply IS NULL OR m.reply ="
+          + " '')")
+  long countUnrepliedMessagesForAgent(@Param("agent") User agent);
 }
